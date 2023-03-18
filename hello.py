49947c927579c295acb1d2faf2741250b91eed48
cedickey <dickec08@gmail.com>
2023-03-17 22:52:56 -0500

    Added a new function and new print command

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-msg = "Hello World"
-print(msg)
-print("A new line in our file!")
+def cylinder_volume(radius, height):
+    volume = (3.14) * radius**2 * height
+    return volume
+
+prompt = input('Enter the radius and height of the cylinder: ')
+print("The volume of the cylinder is:", {cylinder_volume})
