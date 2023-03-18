5f8d5e70f61631caa5fbcce5969263fdc780adb3
cedickey <dickec08@gmail.com>
2023-03-17 23:14:32 -0500

    Don't care

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-msg = "Hello World"
-print(msg)
-print("A new line in our file!")
-print("This line doesn't do anything.")
-=======
 def cylinder_volume(radius, height):
     volume = (3.14) * radius**2 * height
     return volume
@@ -11,4 +5,3 @@ def cylinder_volume(radius, height):
 prompt = input('Enter the radius and height of the cylinder: ')
 print("The volume of the cylinder is:", {cylinder_volume})
 print("This doesn't do anything.")
->>>>>>> hello-world-images
